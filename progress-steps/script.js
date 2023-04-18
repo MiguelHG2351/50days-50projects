58706e6f2dfa7ea06627e4c3138a19ce6abf631d
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-04-18 14:13:37 -0600

    project 2: otra forma interesante :0

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -7,5 +7,12 @@ $numbers.forEach($number => {
     const position = Array.prototype.findIndex.call($numbers, (number) => number === $number)
     console.log(`Position: ${position}, currentValue: ${value}`)
     $container.style.setProperty('--count', position)
+
+    $numbers.forEach((_$number, index) => {
+      if ( index <= position) {
+        return _$number.classList.add('active')
+      }
+      return _$number.classList.remove('active')
+    })
   })
 })
