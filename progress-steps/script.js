a594f81cd2e1dbdfd0420f75a03eca985050c6c5
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-04-18 20:50:46 -0600

    project 2: La forma correcta de resolver pero aún le faltan detalles

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,6 +1,55 @@
 const $numbers = document.querySelectorAll('.number')
 const $container = document.querySelector('.container')
 
+const $prevBtn = document.querySelector('#prevBtn')
+const $nextBtn = document.querySelector('#nextBtn')
+
+$nextBtn.addEventListener('click', function() {
+  let position = $container.style.getPropertyValue('--count').trim() - 0
+  if (position === 3) {
+    return
+  }
+  console.log(`currentValue: ${position}`)
+  position+=1;
+  $container.style.setProperty('--count', position)
+
+  if (position > 0) {
+    $prevBtn.removeAttribute('disabled')
+  }
+  if (position === 3) {
+    this.setAttribute('disabled', true)
+  }
+
+  $numbers.forEach((_$number, index) => {
+    if ( index <= position) {
+      return _$number.classList.add('active')
+    }
+    return _$number.classList.remove('active')
+  })
+})
+
+$prevBtn.addEventListener('click', function() {
+  let position = $container.style.getPropertyValue('--count').trim() - 0
+  if (position === 0) {
+    return
+  }
+  console.log(`currentValue: ${position}`)
+  position-=1;
+  $container.style.setProperty('--count', position)
+
+  if (position === 0) {
+    this.setAttribute('disabled', true)
+    $nextBtn.removeAttribute('disabled')
+  }
+
+  $numbers.forEach((_$number, index) => {
+    if ( index <= position) {
+      return _$number.classList.add('active')
+    }
+    return _$number.classList.remove('active')
+  })
+})
+
 $numbers.forEach($number => {
   $number.addEventListener('click', () => {
     const value = $container.style.getPropertyValue('--count').trim() - 0
