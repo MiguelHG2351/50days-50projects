fefde30eae2effacb9d95914437de07b449fd9cc
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-04-25 12:20:49 -0600

    project 4: the end :D

---
+++ hidden-search/script.js
@@ -0,0 +1,6 @@
+const labelSearch = document.querySelector('.label-search');
+const inputContainer = document.querySelector('.input-container');
+
+labelSearch.addEventListener('click', () => {
+  inputContainer.classList.toggle('active');  
+})
