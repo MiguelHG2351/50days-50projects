128df50efe092b95eac056f6774b7ca095ddc1d6
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-04-25 11:48:28 -0600

    project 3: the end xd

--- rotating-nav-animation/script.js
+++ rotating-nav-animation/script.js
@@ -1,7 +1,12 @@
-const btns = document.querySelectorAll('.btn-icon')
+const btns = document.querySelectorAll('.btn-icon span')
+const btnMenu = document.querySelector('.btn-menu')
+const $main = document.querySelector('main')
+const sidenav = document.querySelector('.sidenav')
 
 btns.forEach(btn => {
   btn.addEventListener('click', () => {
-    console.log('click')
+    btnMenu.classList.toggle('active')
+    $main.classList.toggle('active')
+    sidenav.classList.toggle('active')
   })
 })
