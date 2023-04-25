05c0a4737645997c462aac91e25fe11b6bcfdd84
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-04-25 12:46:50 -0600

    project 4: se hizo solito con copilot :,v

---
+++ blurry-loading/script.js
@@ -0,0 +1,25 @@
+(() => {
+    const loadText = document.querySelector('.loading-text')
+    const bg = document.querySelector('.bg')
+
+    let load = 0
+    let int = setInterval(blurring, 20)
+
+    function blurring() {
+        load++
+
+        if(load > 99) {
+            clearInterval(int)
+        }
+
+        loadText.innerText = `${load}%`
+        loadText.style.opacity = scale(load, 0, 100, 1, 0)
+        // use backdrop filter
+        bg.style.backdropFilter = `blur(${scale(load, 0, 100, 20, 0)}px)`
+    }
+
+    // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+    function scale(number, inMin, inMax, outMin, outMax) {
+        return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
+    }
+})()
